5f9e685233594819215bd91172b4066fc27d1311
Mathieu Deslauriers <43347638+mdeslauriers-sigmund@users.noreply.github.com>
2020-02-14 11:34:58 -0500

    Argument pour le pain

--- baptiste.js
+++ baptiste.js
@@ -1,4 +1,5 @@
-function baptiste() {
+function baptiste(bread) {
+  if (typeof bread === 'chocolatine') throw Error("On dit pain au chocolat!");
   if (cors) return cqrs();
   var impro = npm.run("build", {cshtml:true});
   if (!impro) return Azure.ADB2C();
